c74bbcb02d3da17d344c317ebe4094ef68f40e56
sabernal <sabernal@asesofware.com>
2020-04-03 01:52:37 -0500

    deploy

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('covid-coins app is running!');
+    expect(page.getTitleText()).toEqual('covid-stat app is running!');
   });
 
   afterEach(async () => {
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'covid-coins'`, () => {
+  it(`should have as title 'covid-stat'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('covid-coins');
+    expect(app.title).toEqual('covid-stat');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('covid-coins app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('covid-stat app is running!');
   });
 });
